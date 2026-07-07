72b71e14d5113ab47ccb3f8206782701e8a30618
MatchMates Fix <matchmates@fix.com>
2026-07-07 05:03:03 +0000

    fix: split merged import lines in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react'import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
 import type { Match } from '../types'
 
