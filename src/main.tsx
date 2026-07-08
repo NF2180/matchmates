3a86d4bf2850e9f69f7a199433571225bc20b5e8
MatchMates <matchmates@fix.com>
2026-07-08 08:26:09 +0000

    fix: unregister old service workers on app load

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,13 @@
 // MatchMates v2.1 - 2026-07-08
+// Unregister any old service workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister()
+    }
+  })
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
